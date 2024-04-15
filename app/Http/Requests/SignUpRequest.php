2bd3a92e020f5e7434b5bcf51254a69fb273b550
Jawad Taki alden <jawad.taki.alden2002@gmail.com>
2024-04-15 05:44:20 -0400

    try to fix

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -28,7 +28,7 @@ class SignUpRequest extends FormRequest
             'phone' => 'required|min:10|max:10|unique:users,phone|regex:/^09[0-9]*$/',
             'password' => 'required|min:7|max:26',
             'image' => 'image|mimes:png,jpg,jpeg|max:5120',
-            'device_id' => ['required' , Rule::unique('users' , 'device_id')],
+            'device_id' => 'required|unique:users,device_id',
             'device_notification_id' => 'required'
         ];
     }
